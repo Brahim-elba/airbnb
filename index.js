7a1513a12cd83c8e0c9ce8c704b0df133a4571c7
Brahim-elba <brahim.elbarghouti@outlook.fr>
2021-04-18 13:07:12 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// Installation et import des packages Express, Express-Formidable, Mongoose, UID2, Crypto-js, Cors et Helmet
+const express = require("express");
+const expressFormidable = require("express-formidable");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const helmet = require("helmet");
